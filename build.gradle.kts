66bc91776348805b75bc9120dce176c7d570e335
AtomIsHere <archietomoconnor@gmail.com>
2023-10-01 11:11:41 +0000

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    `java-library`
+    id("io.papermc.paperweight.userdev") version "1.5.5"
+    id("xyz.jpenilla.run-paper") version "2.2.0"
+}
+
+group = "com.github.atomishere"
+version = "1.0-SNAPSHOT"
+description = "A plugin which aims to add RPG elements into minecraft"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    paperweight.paperDevBundle("1.20.2-R0.1-SNAPSHOT")
+}
+
+tasks {
+    assemble {
+        dependsOn(reobfJar)
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+
+        options.release.set(17)
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+        val props = mapOf(
+                "name" to project.name,
+                "version" to project.version,
+                "description" to project.description,
+                "apiVersion" to "1.20"
+        )
+        inputs.properties(props)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
---
+++ src/main/java/com/github/atomishere/atomrpg/AtomRPG.java
@@ -0,0 +1,15 @@
+package com.github.atomishere.atomrpg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AtomRPG extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
