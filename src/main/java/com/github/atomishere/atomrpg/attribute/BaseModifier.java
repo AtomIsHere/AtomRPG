a07bcaa1c706cb3daa46c473749e5cbc71d5b803
AtomIsHere <archietomoconnor@gmail.com>
2023-10-21 00:17:40 +0000

    Rename a variable to make it make more sense

--- src/main/java/com/github/atomishere/atomrpg/attribute/BaseModifier.java
+++ src/main/java/com/github/atomishere/atomrpg/attribute/BaseModifier.java
@@ -1,4 +1,4 @@
 package com.github.atomishere.atomrpg.attribute;
 
-public record BaseModifier(String name, Operation operation, /* this is ignored in some contexts, such as adding a base player value */ ItemSlot slot, double value) {
+public record BaseModifier(String id, Operation operation, /* this is ignored in some contexts, such as adding a base player value */ ItemSlot slot, double value) {
 }
